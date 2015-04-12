5e0a83e5951bb28c674926abfa56843a6993358d
Byron Appelt <byron.appelt@gmail.com>
2015-04-12 22:51:27 +0000

    fixed temp and added humidity

--- temp.py
+++ temp.py
@@ -4,26 +4,27 @@ import time
 import httplib
 import urllib, urllib2
 from datetime import datetime
-from Adafruit_Python_DHT import Adafruit_DHT
+import Adafruit_DHT
 
 station_id = 'KTXARLIN73'
 password = "M*qw1%PCF!"
 
 def read_humid_temp():
-  humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.AM2302, 4)
+  humidity, temp_c = Adafruit_DHT.read_retry(Adafruit_DHT.AM2302, 4)
+  temp_f = temp_c * 9.0 / 5.0 + 32.0
+  return { 'temp_f': temp_f, 'temp_c': temp_c, 'humidity': humidity }
 
-def upload_data():
+def upload_data(data):
     try:
-        temp = read_temp()
         conn = httplib.HTTPConnection("rtupdate.wunderground.com")
-        path = "/weatherstation/updateweatherstation.php?ID=" + station_id + "&PASSWORD=" + password + "&dateutc=" + urllib.quote(str(datetime.utcnow())) + "&tempf=" + str(temp) + "&softwaretype=RaspberryPi&action=updateraw"
+        path = "/weatherstation/updateweatherstation.php?ID=" + station_id + "&PASSWORD=" + password + "&dateutc=" + urllib.quote(str(datetime.utcnow())) + "&tempf=" + str(data['temp_f']) + "&humidity=" + str(data['humidity']) + "&softwaretype=RaspberryPi&action=updateraw"
         print path
         conn.request("GET", path)
         res = conn.getresponse()
         #res = urllib2.urlopen(path).read()
         # checks whether there was a successful connection (HTTP code 200 and content of page contains "success")
         if (int(res.status) == 200):
-   	    print "Successful Upload\nTemp:"
+   	    print "Successful Upload"
         else:
             print "Upload not successful. %i" % res.status
     except IOError as e: #in case of any kind of socket error
@@ -31,5 +32,6 @@ def upload_data():
 	print e
 	
 while True:
-	print read_humid_temp()
+        data = read_humid_temp()
+        upload_data(data)
 	time.sleep(600)
