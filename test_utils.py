415384a5442eb24fbba868cadbb122e0576e4ac0
Byron Appelt <byron.appelt@gmail.com>
2015-04-16 17:27:24 -0500

    testing build failure

--- test_utils.py
+++ test_utils.py
@@ -6,7 +6,7 @@ class WeatherUtilsTestCase(unittest.TestCase):
 
     def test_c_to_f(self):
         """Test Farenheit to Celsius Conversion"""
-        self.assertEquals(32, weather_utils.c_to_f(0))
+        self.assertEquals(33, weather_utils.c_to_f(0))
         self.assertEquals(212, weather_utils.c_to_f(100))
         self.assertEquals(-25.6, weather_utils.c_to_f(-32))
 
